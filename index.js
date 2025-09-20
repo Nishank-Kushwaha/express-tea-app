173bad02e3cadb2ff035ff0fb921d3a3473ed1f7
Nishank-Kushwaha <kushwahanishank01@gmail.com>
2025-09-20 18:13:10 +0530

    add dotenv for environment variable management and update .gitignore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 
@@ -64,7 +65,7 @@ app.delete("/teas/:id", (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`✔️ Server is running on http://127.0.0.1:${PORT}`);
